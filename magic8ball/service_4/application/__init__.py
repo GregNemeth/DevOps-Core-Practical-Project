d466bc444e0c2d46cc2ea38071c16e8df57587ac
Greg Nemeth <GNemeth@qa.com>
2021-08-09 17:10:54 +0000

    GN:app working with sql volume

--- magic8ball/service_4/application/__init__.py
+++ magic8ball/service_4/application/__init__.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' #f"mysql+pymysql://root:{os.getenv('MYSQL_ROOT_PASSWORD')}@magic-db:3306/{os.getenv('MYSQL_DATABASE')}"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://root:{os.getenv('MYSQL_ROOT_PASSWORD')}@magic-db:3306/{os.getenv('MYSQL_DATABASE')}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
