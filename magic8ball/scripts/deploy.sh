6097037d46bb3f6f430414545951bade7fb9fb2b
Greg Nemeth <GNemeth@qa.com>
2021-08-11 15:28:07 +0000

    GN:testing deploy

--- magic8ball/scripts/deploy.sh
+++ magic8ball/scripts/deploy.sh
@@ -3,5 +3,5 @@
 rsync docker-compose.yaml swarm-manager:
 
 ssh swarm-manager << EOF
-docker stack deploy --compose-file docker-compose.yaml magic8ball
+export MYSQL_DATABASE=$MYSQL_DATABASE, MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD && docker stack deploy --compose-file docker-compose.yaml magic8ball
 EOF
