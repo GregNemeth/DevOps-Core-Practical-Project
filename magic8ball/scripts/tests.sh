1dcc8eeddee00bb5f096b6d77f6631685e75cfef
Gergely Nemeth <86304644+GregNemeth@users.noreply.github.com>
2021-08-15 17:21:29 +0100

    Update tests.sh

--- magic8ball/scripts/tests.sh
+++ magic8ball/scripts/tests.sh
@@ -3,9 +3,9 @@
 python3 -m venv venv
 
 source venv/bin/activate
-
+pip3 install --upgrade pip
+pip3 install --upgrade setuptools
 pip3 install -r requirements.txt
-
 
 python3 -m pytest --cov=. --cov-report xml -v
 
